3d5750ba451c1493f42c7773b1f6e007f7fa9b18
dnt2git <dnt2git@users.noreply.github.com>
2023-07-17 00:58:35 +0300

    Добави линк към stackoverflow където се описва datetime parsing

--- bus_models/преглед-на–данните.py
+++ bus_models/преглед-на–данните.py
@@ -14,6 +14,7 @@ from datetime import datetime, timedelta
 def parse_datetime(timestamp):
     '''
     Превръща базираната на 1900-01-01 timestamp ISO форматирана дата
+    Източник: https://stackoverflow.com/questions/68482380/strange-date-format-conversion
 
     Параметри
     ----------
