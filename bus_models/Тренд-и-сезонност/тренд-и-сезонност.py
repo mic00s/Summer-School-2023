b19ef8dfa1f2d519c5a700449484d269c0e3aa0f
dnt2git <dnt2git@users.noreply.github.com>
2023-07-18 11:38:18 +0300

    Добави код за графики на Partial Autocorrelation (PACF)

--- bus_models/Тренд-и-сезонност/тренд-и-сезонност.py
+++ bus_models/Тренд-и-сезонност/тренд-и-сезонност.py
@@ -10,11 +10,11 @@ import pandas as pd
 import numpy as np
 from scipy.signal import lombscargle
 import matplotlib.pyplot as plt
-from statsmodels.graphics.tsaplots import plot_acf
-from statsmodels.tsa.stattools import acf
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+from statsmodels.tsa.stattools import acf, pacf
 
-
-data_csv = 'https://raw.githubusercontent.com/mic00s/Summer-School-2023/main/extended_data/bus_data_w_iso_dates_and_delta_times.csv'
+# data_csv = 'https://raw.githubusercontent.com/mic00s/Summer-School-2023/main/extended_data/bus_data_w_iso_dates_and_delta_times.csv'
+data_csv = '/home/dnt2/Spielplatz/Семково–2023/extended_data/bus_data_w_iso_dates_and_delta_times.csv'
 
 datetime_columns = ['sched_1_355', 'sched_2_1035', 'sched_3_418', 'sched_4_2543',\
                 'stop_1_355',  'stop_2_1035',  'stop_3_418',  'stop_4_2543']
@@ -23,12 +23,19 @@ data = pd.read_csv(data_csv, parse_dates=datetime_columns)
 
 
 
-lags = 30 
+lags = 30
+
+#  ===  ACF  ===
+
 delta_t = 't_stop1_to_stop2'
 time_series = data[delta_t].values
 acf_values = acf(time_series, nlags=lags)
 
 plot_acf(time_series, lags=lags)
+plt.hlines( .2, 0, lags, color='k', ls='--', lw=1)
+plt.hlines(-.2, 0, lags, color='k', ls='--', lw=1)
+ax = plt.gca()
+ax.set_ylim([-.25,1.05])
 plt.xlabel('Lag (отчети)')
 plt.ylabel('Autocorrelation')
 plt.title('Autocorrelation Function (ACF) of {}'.format(delta_t))
@@ -39,6 +46,10 @@ time_series = data[delta_t].values
 acf_values = acf(time_series, nlags=lags)
 
 plot_acf(time_series, lags=lags)
+plt.hlines( .2, 0, lags, color='k', ls='--', lw=1)
+plt.hlines(-.2, 0, lags, color='k', ls='--', lw=1)
+ax = plt.gca()
+ax.set_ylim([-.25,1.05])
 plt.xlabel('Lag (отчети)')
 plt.ylabel('Autocorrelation')
 plt.title('Autocorrelation Function (ACF) of {}'.format(delta_t))
@@ -49,6 +60,53 @@ time_series = data[delta_t].values
 acf_values = acf(time_series, nlags=lags)
 
 plot_acf(time_series, lags=lags)
+plt.hlines( .2, 0, lags, color='k', ls='--', lw=1)
+plt.hlines(-.2, 0, lags, color='k', ls='--', lw=1)
+ax = plt.gca()
+ax.set_ylim([-.25,1.05])
 plt.xlabel('Lag (отчети)')
 plt.ylabel('Autocorrelation')
 plt.title('Autocorrelation Function (ACF) of {}'.format(delta_t))
+
+#  ===  PACF  ===
+
+delta_t = 't_stop1_to_stop2'
+time_series = data[delta_t].values
+acf_values = pacf(time_series, nlags=lags)
+
+plot_pacf(time_series, lags=lags)
+plt.hlines( .2, 0, lags, color='k', ls='--', lw=1)
+plt.hlines(-.2, 0, lags, color='k', ls='--', lw=1)
+ax = plt.gca()
+ax.set_ylim([-.25,1.05])
+plt.xlabel('Lag (отчети)')
+plt.ylabel('Partial Autocorrelation')
+plt.title('Partial Autocorrelation Function (PACF) of {}'.format(delta_t))
+
+
+delta_t = 't_stop2_to_stop3'
+time_series = data[delta_t].values
+pacf_values = pacf(time_series, nlags=lags)
+
+plot_pacf(time_series, lags=lags)
+plt.hlines( .2, 0, lags, color='k', ls='--', lw=1)
+plt.hlines(-.2, 0, lags, color='k', ls='--', lw=1)
+ax = plt.gca()
+ax.set_ylim([-.25,1.05])
+plt.xlabel('Lag (отчети)')
+plt.ylabel('Partial Autocorrelation')
+plt.title('Partial Autocorrelation Function (PACF) of {}'.format(delta_t))
+
+
+delta_t = 't_stop3_to_stop4'
+time_series = data[delta_t].values
+pacf_values = pacf(time_series, nlags=lags)
+
+plot_pacf(time_series, lags=lags)
+plt.hlines( .2, 0, lags, color='k', ls='--', lw=1)
+plt.hlines(-.2, 0, lags, color='k', ls='--', lw=1)
+ax = plt.gca()
+ax.set_ylim([-.25,1.05])
+plt.xlabel('Lag (отчети)')
+plt.ylabel('Partial Autocorrelation')
+plt.title('Partial Autocorrelation Function (PACF) of {}'.format(delta_t))
